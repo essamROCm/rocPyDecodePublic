9a4c4979440c3fe03e5df100f52a83c579447903
Essam Aly <ealy@amd.com>
2024-11-08 16:33:19 -0800

    The changes made to replace using "ssh and file copy/merge" across the network with the built-in MPI-IO which reduces the bandwidth load and allows easier/faster scalability when used on high count of nodes.

--- samples/videodecode_mpi.py
+++ samples/videodecode_mpi.py
@@ -1,3 +1,4 @@
+import torch
 import pyRocVideoDecode.decoder as dec
 import pyRocVideoDecode.demuxer as dmx
 import datetime
@@ -7,99 +8,71 @@ import os.path
 from mpi4py import MPI	
 import subprocess
 import av
+import numpy as np
 
+# This example uses MPI to distribute rocPyDecode decoding task, each node to decode
+# and save an equal section of the total count of frames from the input video file.
+# The example also uses MPI-IO to allow each node to save its portion of the
+# task in the output file utilizing the built it IO mechanism MPI offers.
 
 def Decoder(
         input_file_path,
-        output_file_path,
-        usr_dbg,
-        use_ssh
+        output_file_path
         ):
 
     # mpi
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
-    temp_frames_folder = 'temp_frames_folder/'
 
-    # if use_ssh
-    if use_ssh and rank == 0:
-        print('\n','*'*75, "\n * Using -ssh assumes that you have setup your nodes appropriately")
-        print(" * to successfully communicate with the base node 0 or with each other.\n", '*'*75,'\n')
-        
-    # using ssh allows scale across many external nodes (in this example)
-    if use_ssh and (rank == 0):
-        my_user_name = subprocess.run("whoami", shell=True, capture_output=True, text=True).stdout.strip()
-        my_ip_addrs  = subprocess.run("hostname -I | awk '{print $1}'", shell=True, capture_output=True, text=True).stdout.strip()
-        node0_folder  = subprocess.run("pwd", shell=True, capture_output=True, text=True).stdout.strip()
-        os.makedirs(temp_frames_folder, exist_ok=True)
-        destination_ssh_folder = my_user_name+'@'+my_ip_addrs+':'+node0_folder+'/'+temp_frames_folder
-        print(f"destination_ssh_folder: {destination_ssh_folder}")
-    else:        
-        destination_ssh_folder = None
-
-    # all know dest if exist
-    if output_file_path is not None and use_ssh:
-        destination_ssh_folder = comm.bcast(destination_ssh_folder, root=0)
-                
-    # TODO (rocDecode):
+    # TODO (@rocDecode): -> Get count of frames:
     # The rocDecode demuxer/parser needs to go through the  
     # entire stream without decoding, to get the count of
-    # frames till this API being implemented, use av to ..
+    # frames. Until such an API being implemented, use av for now
 
-    # .. get count of frames
+    # Get count of frames
     if rank == 0:        
         container = av.open(input_file_path)               # Open the video file
         frames_total = container.streams.video[0].frames   # Get total number of frames from first video stream
     else:
         frames_total = None
 
-    # demuxer instance
+    comm.barrier() # synchronize all processes
+
+    # Create demuxer instance
     demuxer = dmx.demuxer(input_file_path)
-    # get the used coded id
+    # Get the codec id
     codec_id = dec.GetRocDecCodecID(demuxer.GetCodecId())
-    # decoder instance
+    # Create decoder instance
     viddec = dec.decoder(codec_id)
-    # check if codec is supported
+    # Check if codec is supported, exit otherwise
     if (viddec.IsCodecSupported(0, codec_id, demuxer.GetBitDepth()) == False):
         print("ERROR: Codec is not supported on this GPU " + viddec.GetGpuInfo().device_name)
         exit()
 
-    # ----
-    # MPI
-    # ----
+    # ---------
+    # MPI Setup
+    # ---------
  
-    # all ranks to know what is 'frames_total'
+    # TO: all ranks to know what is 'frames_total' FROM: node 0
     frames_total = comm.bcast(frames_total, root=0)
  
-     # calc for all
+     # THIS rank portion of the task, calc that
     frames_remain = frames_total % size
     each_proc_frame_count = int((frames_total-frames_remain)/size)
     this_rank_start = rank * each_proc_frame_count
     this_rank_end  = this_rank_start + each_proc_frame_count - 1 
 
-    # add 'frames_remain' to last rank
+    # Add 'frames_remain' to last rank
     if (rank == (size-1)):
         this_rank_end += frames_remain # last rank takes care of the residue
 
-    # no need for file operations if user do not want to save frames, save time, performance
-    save_output_file_path = None
-    if rank == 0:
-        if output_file_path is not None:
-            save_output_file_path = output_file_path
-
-    # serialize the output frames files (in case we used it)
-    output_file_path = f"output_frames_{rank}_2048x1024_8bit_420.yuv" 
-    flush_mode = 1
-    # set reconfiguration params based on user arguments
-    viddec.SetReconfigParams(flush_mode, output_file_path)
-
-    # all ranks to know if we saved is temp filename
-    save_decoded_section = False
-    if rank == 0:        
-        if save_output_file_path is not None:
-            save_decoded_section = True
-    save_decoded_section = comm.bcast(save_decoded_section, root=0)
+    # Set reconfiguration params if using -o
+    save_frames = False
+    if output_file_path is not None:
+        save_frames = True
+        flush_mode = 1
+        viddec.SetReconfigParams(flush_mode, output_file_path)
 
     # -----------------
     # The decoding loop
@@ -107,36 +80,58 @@ def Decoder(
 
     frame_index = 0
     total_dec_time = 0.0
+    save_file_prerequisite = False
+    saved_frames_index = this_rank_start
 
     while True:
 
-        start_time = datetime.datetime.now() # whole frame
+        start_time = datetime.datetime.now()
         packet = demuxer.DemuxFrame()
         n_of_frames_returned = viddec.DecodeFrame(packet)
 
         for i in range(n_of_frames_returned):
 
-            viddec.GetFrameYuv(packet)
+            viddec.GetFrameYuv(packet, False)
+            frame_data = torch.from_dlpack(packet.ext_buf[0].__dlpack__(packet)).cpu().numpy()
 
             # only save when requested, and only save the assigned frames to this rank
-            if (save_decoded_section and (output_file_path is not None)):
-                if ((frame_index >= this_rank_start) and (frame_index <= this_rank_end)): # only the assigned section of frames
-                    viddec.SaveFrameToFile(output_file_path, packet.frame_adrs)
+            if (save_frames and (frame_index>=this_rank_start) and (saved_frames_index<=this_rank_end)):
+                # Setup 'Save Frame to File' Configuration One Time
+                if save_file_prerequisite is not True:
+                    width = float(viddec.GetStride())          # Width of each frame in pixels
+                    height = float(viddec.GetHeight())         # Height of each frame in pixels
+                    bit_depth = float(viddec.GetBitDepth())    # Bit depth of each pixel (e.g., 8, 16, 32)
+                    frame_size_bytes = int((width * height * 1.5 * bit_depth) // 8.0)  # Calculate frame size in bytes, 1.5 for the UV to be included in NV12 format
+                    save_file_prerequisite = True
+                    # Open the file for writing in parallel
+                    try:
+                        f = MPI.File.Open(comm, output_file_path, MPI.MODE_WRONLY | MPI.MODE_CREATE)
+                    except Exception as e:
+                        print(f"Process {rank} encountered an error: {e}")
+
+                # Calculate offset based on frame number
+                offset = saved_frames_index * frame_size_bytes
+                f.Write_at(offset, frame_data.ravel()) # Independent File Pointer
+                saved_frames_index += 1
 
             # release frame
             viddec.ReleaseFrame(packet)
 
-        # measure time after completing a whole frame
+        # Measure time after completing a whole frame
         end_time = datetime.datetime.now()
         time_per_frame = end_time - start_time
         total_dec_time = total_dec_time + time_per_frame.total_seconds()
 
-        # increament frames counter
+        # Increment frames counter
         frame_index += n_of_frames_returned
 
-        # stop if no more to demux/decode
+        # Stop if no more to demux/decode
         if (packet.bitstream_size <= 0):  # EOF: no more to decode
             break
+
+    # close file if we have opened it
+    if save_frames:
+        f.Close()
 
     # beyond the decoding loop
     frame_index += viddec.GetNumOfFlushedFrames()        
@@ -144,60 +139,29 @@ def Decoder(
     # collect total elapsed time in all processes
     total_time_elapsed = comm.gather(total_dec_time, root=0)
 
-    # collect session overhead from all processes
-    session_overhead = comm.gather(viddec.GetDecoderSessionOverHead(rank), root=0)
-
-    # copy resultled file from local node to main node 0, then delete it, only if ssh used
-    if (save_decoded_section and use_ssh):
-        cmmnd = 'scp '+output_file_path+' '+destination_ssh_folder+output_file_path
-        subprocess.run(cmmnd, shell=True, check=True)
-
-    # make sure all finished their share of the job
-    comm.barrier() # synchronize all processes
-
-    # if user used output file then save all in one
-    if rank == 0:
-        if save_output_file_path is not None:
-            files_list = ''
-            for i in range(size):
-                files_list += f"output_frames_{i}_2048x1024_8bit_420.yuv "
-            
-            cat_cmd = 'cat '+files_list+' > '+save_output_file_path
-            rm_cmd = 'rm '+files_list                        
-            
-            print("\nConstructing YUV file from nodes results and removing temp files ..")
-            
-            if use_ssh is not True:
-                subprocess.run(cat_cmd, shell=True, check=True)
-                subprocess.run(rm_cmd, shell=True, check=True)
-            else: # this is happening here locally at node 0
-                chg_dir = " \"cd "+node0_folder+'/'+temp_frames_folder
-                subprocess.run(chg_dir +" && "+ cat_cmd + "\"", shell=True, check=True)
-                # you may need to keep the temp files, if so comment next line
-                subprocess.run(chg_dir + " && " + rm_cmd + "\"", shell=True, check=True)
-        else:
-            print("No output-file was specified..\n")
-
-        # report time
-        frame_index = frame_index * size # we decoded 'size' times, depends on the -n mpirun/mpiexec param
-        total_dec_time = sum(total_time_elapsed)/size # average of all processes time
-        time_per_frame = (total_dec_time / frames_total) * 1000 # we decoded ALL anyway
-        time_per_frame -= (sum(session_overhead) / frame_index) # remove the overhead (if any)
-        frame_per_second = frame_index / total_dec_time
-        print("info: avg decoding time per frame: " +"{0:0.2f}".format(round(time_per_frame, 2)) + " ms")
-        print("info: avg frame per second: " +"{0:0.2f}".format(round(frame_per_second,2)) +"\n")
-
-        # for user dbg
-        if usr_dbg:
-            print(f"\nElapsed time reported by all processes: {total_time_elapsed}")
-            print(f"Highest elapsed time (actual decoding time): {max(total_time_elapsed)}")
-            print("Total processors: ", size)
-            print("Frames per proc:  ", each_proc_frame_count)
-            print("Frames Remaining: ", frames_remain)
-
     # done using MPI
     MPI.Finalize
 
+    # report time
+    if (rank == 0):
+        frame_index = frame_index * size # we decoded 'size' times, depends on the -n mpirun/mpiexec param
+        if (total_time_elapsed is not None):
+            total_dec_time = sum(total_time_elapsed)/size # average of all processes time
+        time_per_frame = (total_dec_time / frames_total) * 1000 # we decoded ALL anyway
+        frame_per_second = frame_index / total_dec_time
+
+        print("\ninfo: avg decoding time per frame: " +"{0:0.2f}".format(round(time_per_frame, 2)) + " ms")
+        print("info: avg frame per second: " +"{0:0.2f}".format(round(frame_per_second,2)) +"\n")
+
+        print(f"Total nodes count executed: {size}\n")
+
+        # for user dbg
+        # if (total_time_elapsed is not None):
+        #     print(f"\nElapsed time reported by all processes: {total_time_elapsed}")
+        #     print(f"Highest elapsed time (actual decoding time): {max(total_time_elapsed)}")
+        # print("Total processors: ", size)
+        # print("Frames per proc:  ", each_proc_frame_count)
+        # print("Frames Remaining: ", frames_remain)
 
 if __name__ == "__main__":
 
@@ -216,21 +180,6 @@ if __name__ == "__main__":
         type=str,
         help='Output File Path - optional',
         required=False)
-    parser.add_argument(
-        '-dbg',
-        '--debug',
-        type=str,
-        default='no',
-        choices=['yes', 'no'],
-        help='Print out some debug information')
-    parser.add_argument(
-        '-ssh',
-        '--use_ssh',
-        type=str,
-        default='no',
-        choices=['yes', 'no'],
-        help='Use ssh scp to copy decoded frames, this example way of consumption'
-        )
 
     try:
         args = parser.parse_args()
@@ -240,12 +189,8 @@ if __name__ == "__main__":
     # get params
     input_file_path = args.input
     output_file_path = args.output
-    usr_dbg = args.debug.upper()
-    use_ssh = args.use_ssh.upper()
-    
+
     # handle params
-    usr_dbg = True if usr_dbg == 'YES' else False
-    use_ssh = True if use_ssh == 'YES' else False
     if not os.path.exists(input_file_path):  # Input file (must exist)
         print("ERROR: input file doesn't exist.")
         exit()
@@ -253,7 +198,5 @@ if __name__ == "__main__":
     # call main routine
     Decoder(
         input_file_path,
-        output_file_path,
-        usr_dbg,
-        use_ssh
+        output_file_path
     )
