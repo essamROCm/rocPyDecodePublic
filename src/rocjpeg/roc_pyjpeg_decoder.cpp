a0e2784dc82a343941de3832e73abf20c07c5132
Essam Aly <essam.aly@amd.com>
2025-04-16 15:46:56 -0700

    Adding PLANAR sample making use of PLANAR output format, correcting PLAN dim

--- src/rocjpeg/roc_pyjpeg_decoder.cpp
+++ src/rocjpeg/roc_pyjpeg_decoder.cpp
@@ -273,9 +273,9 @@ bool Decoder::to_dlpack_tensor(CodeStream* code_stream, PyJpegImages* image) {
     std::string type_str(static_cast<const char*>("|u1"));
     switch(output_format) {
         case ROCJPEG_OUTPUT_RGB_PLANAR: { // each color plane in a channel separately R[0], G[1], and B[2]
-            uint32_t surf_stride[3] = {widths[0], widths[1], widths[2]}; // ROCJPEG_OUTPUT_RGB_PLANAR
+            uint32_t surf_stride[3] = {widths[0], widths[1], widths[2]}; // ROCJPEG_OUTPUT_RGB_PLANAR all same width = img_width
             for(int i=0; i<3; i++) {
-                std::vector<size_t> shape{ static_cast<size_t>(heights[i]), static_cast<size_t>(widths[i]), 3}; // depend on get_widths_heights_from_output_format()
+                std::vector<size_t> shape{ static_cast<size_t>(heights[i]), static_cast<size_t>(widths[i]), 1}; // depend on get_widths_heights_from_output_format()
                 std::vector<size_t> stride{ static_cast<size_t>(surf_stride[i]), 1, 0};
                 // RGB PLANAR using VCN JPEG decoder @ first, second, and third channel of RocJpegImage
                 image->ext_buf[i]->LoadDLPack(shape, stride, bit_depth, type_str, (void *)code_stream->output_image.channel[i], m_device_id); // m_device_id was set at the constructor
@@ -285,7 +285,7 @@ bool Decoder::to_dlpack_tensor(CodeStream* code_stream, PyJpegImages* image) {
         case ROCJPEG_OUTPUT_RGB: // all the image RGB interleaved in one channel [0]
         default: {
             uint32_t surf_stride = widths[0]; // ROCJPEG_OUTPUT_RGB width is * 3 for RGB interleaved
-            std::vector<size_t> shape{ static_cast<size_t>(heights[0]), static_cast<size_t>(widths[0]/3), 3}; // /3 because of the ROCJPEG_OUTPUT_RGB it will be/1 if PLANAR
+            std::vector<size_t> shape{ static_cast<size_t>(heights[0]), static_cast<size_t>(widths[0]/3)}; // widths[0]/3 for ROCJPEG_OUTPUT_RGB
             std::vector<size_t> stride{ static_cast<size_t>(surf_stride), 1, 0}; // python assumes same dim for both shape & strides
             // interleaved RGB using VCN JPEG decoder written to first channel of RocJpegImage
             image->ext_buf[0]->LoadDLPack(shape, stride, bit_depth, type_str, (void *)code_stream->output_image.channel[0], m_device_id); // m_device_id was set at the constructor
